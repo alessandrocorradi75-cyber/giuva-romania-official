1a36df6f07aa32d8892e547f0664dd41cfcc448e
Alessandro <alessandro@example.com>
2026-05-20 00:53:54 +0300

    Move GIUVA project outside OneDrive

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isVercel = process.env.VERCEL === "1";
-
 const nextConfig = {
-  ...(isVercel ? {} : { distDir: ".next-local" }),
   images: {
     formats: ["image/avif", "image/webp"]
   }
 };
+
 export default nextConfig;
